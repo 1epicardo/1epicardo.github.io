471d4e4355c37687224731e02a1944bcc9efd298
Esteban Picardo <epicardo@signumenergy.com.ar>
2023-03-01 08:34:43 -0300

    ajuste menor en js

--- js/function.js
+++ js/function.js
@@ -3,52 +3,47 @@ document.getElementById('m_home').onclick = function () {
     document.getElementById('conocimientos').style.display='none';
 }
 
+
 document.getElementById('m_conocimientos').onclick = function () {
     document.getElementById('conocimientos').style.display='';
     document.getElementById('home').style.display='none';
 }
 
 
-
-document.getElementById('oc_titulo').onclick = function () {
-    document.getElementById('titulo').style.display='none';
-}
-
-
-
 document.getElementById('h_html').onclick = function () {
     document.getElementById('html').style.display='';
 }
+
 
 document.getElementById('oc_html').onclick = function () {
     document.getElementById('html').style.display='none';
 }
 
 
-
 document.getElementById('h_css').onclick = function () {
     document.getElementById('css').style.display='';
 }
+
 
 document.getElementById('oc_css').onclick = function () {
     document.getElementById('css').style.display='none';
 }
 
 
-
 document.getElementById('h_git').onclick = function () {
     document.getElementById('git').style.display='';
 }
+
 
 document.getElementById('oc_git').onclick = function () {
     document.getElementById('git').style.display='none';
 }
 
 
-
 document.getElementById('h_js').onclick = function () {
     document.getElementById('js').style.display='';
 }
+
 
 document.getElementById('oc_js').onclick = function () {
     document.getElementById('js').style.display='none';
